b6c3bcf546f0f3516a3e9c7a68bbea8eb9cbc5a2
M Moronto <T2WYCEMO@GMAIL.com>
2020-09-11 18:23:10 -0400

    Run migration to update schema

--- db/migrate/20200911220737_create_items.rb
+++ db/migrate/20200911220737_create_items.rb
@@ -3,8 +3,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
     create_table :items do |t|
       t.string :description
       t.integer :list_id
-      # Wiring the Schema - The database - Foreign Key
-      # Wire my model with the association.
+
       t.timestamps
     end
   end
