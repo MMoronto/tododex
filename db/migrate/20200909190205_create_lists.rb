de7e0ae89b45912d1b400c2ebf1fe81248f9653d
M Moronto <T2WYCEMO@GMAIL.com>
2020-09-11 18:15:24 -0400

    revise lists db to include description & list_id columns

--- db/migrate/20200909190205_create_lists.rb
+++ db/migrate/20200909190205_create_lists.rb
@@ -1,9 +1,10 @@
 class CreateLists < ActiveRecord::Migration[6.0]
   def change
     create_table :lists do |t|
-      t.string :name
+      t.string :description
+      t.integer :list_id
 
-      t.timestamps
+      t.timestamps null: false
     end
   end
 end
