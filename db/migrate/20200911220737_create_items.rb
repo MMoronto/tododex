dfc70d56d5a70da4e007e4d1487887564739f8ac
M Moronto <T2WYCEMO@GMAIL.com>
2020-09-11 18:21:18 -0400

    add description & list_id cols to Items schema

--- db/migrate/20200911220737_create_items.rb
+++ db/migrate/20200911220737_create_items.rb
@@ -2,7 +2,9 @@ class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
       t.string :description
-
+      t.integer :list_id
+      # Wiring the Schema - The database - Foreign Key
+      # Wire my model with the association.
       t.timestamps
     end
   end
