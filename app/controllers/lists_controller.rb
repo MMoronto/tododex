e5b15aa0ee46ad947055d085e7897b75177aa80e
M Moronto <T2WYCEMO@GMAIL.com>
2020-09-10 21:50:59 -0400

    implement strong params pattern within create method in lists_controller class

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -11,11 +11,16 @@ class ListsController < ApplicationController
   end
 
   def create
-    @list = List.new
+    @list = List.new(list_params)
     @list.name = params[:list][:name]
     @list.save
 
-    redirect_to list_path(@list)
+    redirect_to list_url(@list)
   end
+  private
+
+    def list_params
+      params.require(:list).permit(:name)
+    end
 
 end
